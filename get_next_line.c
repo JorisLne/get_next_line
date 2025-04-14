86999ce12d7f253f229630ea9aa298637d4a09be
JorisLne <joris.laine98@gmail.com>
2025-04-15 00:49:08 +0200

    main

---
+++ get_next_line.c
@@ -0,0 +1,103 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.c                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jlaine <jlaine@student.42.fr>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/06/11 10:33:41 by jlaine            #+#    #+#             */
+/*   Updated: 2024/06/12 18:21:35 by jlaine           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "get_next_line.h"
+
+char	*read_until_newline(int fd, char **buffer)
+{
+	ssize_t	bytes_read;
+	char	*temp_buffer;
+
+	bytes_read = 1;
+	temp_buffer = malloc(BUFFER_SIZE + 1);
+	if (*buffer == NULL)
+		*buffer = ft_strdup("");
+	while (bytes_read && !ft_strchr(*buffer, '\n'))
+	{
+		bytes_read = read(fd, temp_buffer, BUFFER_SIZE);
+		if (bytes_read == -1)
+		{
+			free (temp_buffer);
+			free (*buffer);
+			*buffer = NULL;
+			return (NULL);
+		}
+		temp_buffer[bytes_read] = '\0';
+		*buffer = ft_strjoin(*buffer, temp_buffer);
+	}
+	free (temp_buffer);
+	return (NULL);
+}
+
+char	*extract_line(char **buffer)
+{
+	char	*new_buffer;
+	char	*line;
+
+	if (!ft_strchr(*buffer, '\n'))
+	{
+		line = ft_strdup(*buffer);
+		free (*buffer);
+		*buffer = NULL;
+	}
+	else
+	{
+		line = ft_substr(*buffer, 0, ft_strchr(*buffer, '\n') - *buffer + 1);
+		new_buffer = ft_strdup(ft_strchr (*buffer, '\n') + 1);
+		free (*buffer);
+		*buffer = new_buffer;
+	}
+	return (line);
+}
+
+char	*get_next_line(int fd)
+{
+	static char	*buffer;
+	char		*line_read;
+
+	line_read = NULL;
+	if (fd < 0 || BUFFER_SIZE <= 0)
+		return (NULL);
+	read_until_newline(fd, &buffer);
+	if (buffer && buffer[0] != 0)
+		line_read = extract_line(&buffer);
+	if (buffer && buffer[0] == 0)
+	{
+		free (buffer);
+		buffer = NULL;
+	}
+	return (line_read);
+}
+
+/*
+int main(void)
+{
+  int	fd;
+  char  *next_line;
+  int  	count;
+
+  count = 0;
+  fd = open("example.txt", O_RDONLY);
+  while (1)
+  {
+    next_line = get_next_line(fd);
+    if (next_line == NULL)
+      break ;
+    count++;
+    printf("[%d]:%s", count, next_line);
+	free (next_line);
+    next_line = NULL;
+  }
+  close(fd);
+  return (0);
+}
+*/
